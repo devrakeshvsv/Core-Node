c36e1de878a75d4911473c397d2975d6f8f82c94
rakesh830566 <rakesh830566@gmail.com>
2021-01-09 14:47:28 +0530

    Service 3 tokens

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -29,5 +29,28 @@ helpers.parseJsonToObject = function (str) {
 	}
 };
 
+// Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = function (strLength) {
+	strLength = typeof strLength == 'number' && strLength > 0 ? strLength : false;
+
+	if (strLength) {
+		// Define all the possible character that could go into a string
+		var possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+		// Start the final string
+		var str = '';
+		for (let i = 1; i <= strLength; i++) {
+			// Get random character for the possibleCharacters string
+			var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+			// Append this character to the final string
+			str += randomCharacter;
+		}
+		// Return the final string
+		return str;
+	} else {
+		return false;
+	}
+};
+
 // Export the module
 module.exports = helpers;
